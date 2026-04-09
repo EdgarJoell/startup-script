e62090a387646d44e778c1572a842c51d957ea1e
Edgar Zambrana <edgarjoel0920@gmail.com>
2026-04-09 00:12:27 -0400

    Reformat script to have Homebrew installation at the top but below the arrays. Add step numbers for the loops, reformat indentations.

--- startup.sh
+++ startup.sh
@@ -2,36 +2,6 @@ HOMEBREW_PACKAGES=("git" "node" "ruby" "typescript" "mongosh" "nvm" "postgresql"
 HOMEBREW_CASKS=("webstorm" "rubymine" "intellij-idea" "rider" "visual-studio-code" "jetbrains-toolbox" "datagrip" "pgadmin4" "dbeaver-community" "clion" "pycharm")
 NPM_PACKAGES=("@angular/cli" "typescript" "ts-node" "nodemon" "create-next-app" "react-native-cli" "expo-cli" "electron")
 
-for hbPackage in "${HOMEBREW_PACKAGES[@]}"; do
-  printf "\nInstalling %s...\n" "$hbPackage"
-
-  if ! brew list "$hbPackage" &>/dev/null; then
-    brew install "$hbPackage"
-  else
-    printf "%s already installed, skipping, \n" "$hbPackage"
-  fi
-done
-
-for hbCask in "${HOMEBREW_CASKS[@]}"; do
-  printf "\nInstalling %s...\n" "$hbCask"
-
-  if ! brew list "$hbCask" &>/dev/null; then
-    brew install "$hbCask"
-  else
-    printf "%s already installed, skipping, \n" "$hbCask"
-  fi
-done
-
-for npmPackage in "${NPM_PACKAGES[@]}"; do
-  printf "\nInstalling %s...\n" "$npmPackage"
-
-  if ! brew list "$npmPackage" &>/dev/null; then
-    brew install "$npmPackage"
-  else
-    printf "%s already installed, skipping, \n" "$npmPackage"
-  fi
-done
-
 printf "Beginning Process of setting up Mac machine for development...\n"
 
 printf "\nStep 1: Installing Homebrew to machine...\n"
@@ -48,38 +18,74 @@ else
   echo "Homebrew already installed, skipping."
 fi
 
+printf "\nStep 2: Installing Homebrew Packages\n"
+
+for hbPackage in "${HOMEBREW_PACKAGES[@]}"; do
+  printf "\nInstalling %s...\n" "$hbPackage"
+
+  if ! brew list "$hbPackage" &>/dev/null; then
+    brew install "$hbPackage"
+  else
+    printf "%s already installed, skipping, \n" "$hbPackage"
+  fi
+done
+
+printf "\nStep 3: Installing Homebrew Casks (AKA: IDE's and other Development Application\n"
+
+for hbCask in "${HOMEBREW_CASKS[@]}"; do
+  printf "\nInstalling %s...\n" "$hbCask"
+
+  if ! brew list "$hbCask" &>/dev/null; then
+    brew install "$hbCask"
+  else
+    printf "%s already installed, skipping, \n" "$hbCask"
+  fi
+done
+
+printf "\nStep 4: Installing NPM packages\n"
+
+for npmPackage in "${NPM_PACKAGES[@]}"; do
+  printf "\nInstalling %s...\n" "$npmPackage"
+
+  if ! brew list "$npmPackage" &>/dev/null; then
+    brew install "$npmPackage"
+  else
+    printf "%s already installed, skipping, \n" "$npmPackage"
+  fi
+done
+
 printf "\nStep 6: Installing OpenJDK..."
 
 if ! brew list openjdk &>/dev/null; then
-   INCORRECT_VALUE=1
+  INCORRECT_VALUE=1
 
-   while [ "$INCORRECT_VALUE" -eq 1 ]
-   do
-      read -r -t 10 -p "Which Java version would you like as your main version? Allowed versions: 11, 17, 21, 25 (if no version is specified, this script defaults to 21): " version
+  while [ "$INCORRECT_VALUE" -eq 1 ]
+  do
+    read -r -t 10 -p "Which Java version would you like as your main version? Allowed versions: 11, 17, 21, 25 (if no version is specified, this script defaults to 21): " version
 
-      if [ "$version" = "" ]; then
-         version=21
-      fi
+    if [ "$version" = "" ]; then
+       version=21
+    fi
 
-      if [[ "$version" =~ ^(11|17|21|25)$ ]]; then
-         INCORRECT_VALUE=0
-         echo "Continuing..."
-      else
-         echo "Incorrect value, please try again."
-      fi
-   done
+    if [[ "$version" =~ ^(11|17|21|25)$ ]]; then
+       INCORRECT_VALUE=0
+       echo "Continuing..."
+    else
+       echo "Incorrect value, please try again."
+    fi
+  done
 
-   echo "Installing the OpenJDK's..."
-   brew install openjdk@11
-   brew install openjdk@17
-   brew install openjdk@21
-   brew install openjdk@25
+  echo "Installing the OpenJDK's..."
+  brew install openjdk@11
+  brew install openjdk@17
+  brew install openjdk@21
+  brew install openjdk@25
 
-   printf "\nLinking default JDK version to the version specified...\n"
+  printf "\nLinking default JDK version to the version specified...\n"
 
-   brew link openjdk@"$version"
+  brew link openjdk@"$version"
 else
-   echo "OpenJDK already installed, skipping,"
+  echo "OpenJDK already installed, skipping,"
 fi
 
 printf  "\nPerforming cleanup and finalizing the Startup Script\n"
